1e2e2e1acc85f739dc1a3392280f556d9c953d28
Fernando-Leon <leon.monroy.fernando@gmail.com>
2024-01-23 18:40:26 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'integradora',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
